9a4bc018ffa5b588db24bbc4edee7132ef37c0f7
qsdrqs <qsdrqs@gmail.com>
2020-04-09 16:58:58 +0800

    update

--- autovim.sh
+++ autovim.sh
@@ -2,5 +2,13 @@
 cp .vimrc ..
 cp .vimrc.plugs ..
 cd .vim
-mkdir ~/.vim
-cp -r * ~/.vim/
+if [[ -d "~/.vim" ]]; then
+    if [[ -d "~/.vim/UltiSnips" && -d "~/.vim/colors" ]]; then
+        cp -r ./.vim/UltiSnips/* ~/.vim/UltiSnips/
+        cp -r ./.vim/colors/* ~/.vim/colors/
+    else
+        cp -r * ~/.vim/
+    fi
+else
+    mkdir ~/.vim
+fi
